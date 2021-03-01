765fd90aed5ad95efa927677ecfbe96a0ffd10df
Hans Anderson <me@ha17.com>
2021-03-01 15:19:42 -0600

    updates
    Signed-off-by: Hans Anderson <me@ha17.com>

--- wp/install-plugins.sh
+++ wp/install-plugins.sh
@@ -1,3 +1,3 @@
 docker-compose run --rm wpcli core update --path=/var/www/html
-docker-compose run --rm wpcli plugin install envato-elements elementor header-footer-elementor --activate --path=/var/www/html
+docker-compose run --rm wpcli plugin install envato-elements elementor header-footer-elementor autoptimize all-in-one-wp-migration updraftplus ga-google-analytics jm-twitter-cards cookie-law-info prismatic jwt-authentication-for-wp-rest-api --activate --path=/var/www/html
 docker-compose run --rm wpcli theme install hello-elementor --activate --path=/var/www/html
